493ab2d255b809080b24a2e8a0da19546ce61a99
Benjamin Lawson <lawbe101@yahoo.com>
2018-01-18 13:51:39 -0500

    filled out viewing and updating routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,9 +16,48 @@ router.get('/articles', (req, res) => {
   db.Article
     .find({})
     .then(dbArticle => {
-      res.json(dbArticle)
+      res.json(dbArticle);
     })
     .catch(err => {
       res.json(err);
     })
 })
+
+router.get('/saved', (req, res) => {
+  db.Article
+    .find({ saved: true })
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+})
+
+router.get('/articles/:id', (req, res) => {
+  db.Article
+  .findOne({ _id: req.params.id })
+  .populate('note')
+  .then(dbArticle => {
+    res.json(dbArticle);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+})
+
+router.post('/articles/:id', (req, res) => {
+  db.Note
+  .create(req.body)
+  .then(dbNote => {
+    return db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { note: dbNote._id },
+      { new: true }
+    );
+  })
+  .catch(err => {
+    res.json(err);
+  })
+})
+
